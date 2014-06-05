06b1258feb5c97079246a2468a0b0ff355ddf60e
Christopher <decoster.c@gmail.com>
2014-06-05 19:08:01 +0200

    release 1.1

---
+++ complete.R
@@ -0,0 +1,9 @@
+
+complete <- function(directory, id = 1:332) {
+  files <- paste(getwd(), "/", directory, "/", substr(1000+id,2,4), ".csv", sep = "")
+  df <- na.omit(do.call("rbind", lapply(files, read.csv, header = TRUE)) )
+  df$ID <- factor(df$ID, levels = id)
+  a <- aggregate(Date ~ ID,data = df, FUN = function(x) length(unique(x)))
+  colnames(a)<-c("id","nobs")
+  a
+}
