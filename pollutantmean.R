db00d801c6e7896ade7801050daef5433086009a
Christopher <decoster.c@gmail.com>
2014-06-05 19:00:02 +0200

    release 1

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,20 +1,21 @@
+source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
+submit(manual=TRUE)
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  sapply(paste("~/"+directory, 
-               id, sep=".csv"), 
-         read.csv, header=TRUE,  dec = ".") # etc
-  
-  
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return the mean of the pollutant across all monitors list
+  ## This function returns the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
+  files <- paste(getwd(), "/", directory, "/", substr(1000+id,2,4), ".csv", sep = "")
+  df <- do.call("rbind", lapply(files, read.csv, header = TRUE)) 
+  round(mean(df[[pollutant]], na.rm = TRUE), digits = 3)
 }
+
+complete <- function(directory, id = 1:332) {
+  files <- paste(getwd(), "/", directory, "/", substr(1000+id,2,4), ".csv", sep = "")
+  df <- na.omit(do.call("rbind", lapply(files, read.csv, header = TRUE)) )
+  df$ID <- factor(df$ID, levels = id)
+  a <- aggregate(Date ~ ID,data = df, FUN = function(x) length(unique(x)))
+  colnames(a)<-c("id","nobs")
+  a
+}
+
+
